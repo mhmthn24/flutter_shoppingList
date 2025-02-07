3223473fdae1436ab4641c27a32d5fc80e4538c2
Mehmethan YUMUS <mehmethanyumus@Mehmethans-MacBook-Air.local>
2025-02-07 00:28:38 +0000

    Listeleme ekrani butonlar ve gorseller aktif hale getirildi. Sonraki Gorev -> SharedPreferences depolamasi yapilacak

--- lib/MainPage.dart
+++ lib/MainPage.dart
@@ -18,6 +18,9 @@ class _MainpageState extends State<Mainpage> {
     Urun(urun_id: 4, urun_adi: "Muz", urun_birim: "Adet", urun_adet: 3),
   ];
 
+  List<String> birimListesi = ["Kilogram", "Adet", "Paket", "Litre"];
+  String birim = "Kilogram";
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -43,10 +46,14 @@ class _MainpageState extends State<Mainpage> {
                   child: Column(
                     children: [
                       ElevatedButton(
-                        onPressed: (){},
+                        onPressed: (){
+                          setState(() {
+                            urunEkleDialog(context);
+                          });
+                        },
                         style: ElevatedButton.styleFrom(
                           backgroundColor: Colors.orange,
-                          foregroundColor: Colors.white
+                          foregroundColor: Colors.black
                         ),
                         child: Row(
                           mainAxisSize: MainAxisSize.max,
@@ -55,14 +62,14 @@ class _MainpageState extends State<Mainpage> {
                             Text(
                               "Yeni Ürün Ekle",
                               style: TextStyle(
-                                fontSize: 16,
+                                fontSize: 20,
                                 fontWeight: FontWeight.bold
                               ),
                             ),
                             Icon(
                               Icons.add,
-                              color: Colors.white,
-                              size: 24,
+                              color: Colors.black,
+                              size: 32,
                             )
                           ],
                         ),
@@ -84,7 +91,7 @@ class _MainpageState extends State<Mainpage> {
                         },
                         style: ElevatedButton.styleFrom(
                             backgroundColor: Colors.orange,
-                            foregroundColor: Colors.white
+                            foregroundColor: Colors.black
                         ),
                         child: Row(
                           mainAxisSize: MainAxisSize.max,
@@ -93,14 +100,14 @@ class _MainpageState extends State<Mainpage> {
                             Text(
                               "Listeyi Temizle",
                               style: TextStyle(
-                                  fontSize: 16,
+                                  fontSize: 20,
                                   fontWeight: FontWeight.bold
                               ),
                             ),
                             Icon(
                               Icons.clear_all,
-                              color: Colors.white,
-                              size: 24,
+                              color: Colors.black,
+                              size: 32,
                             )
                           ],
                         ),
@@ -131,89 +138,279 @@ class _MainpageState extends State<Mainpage> {
       mainAxisAlignment: MainAxisAlignment.center,
       mainAxisSize: MainAxisSize.max,
       children: [
-        Container(
-          padding: EdgeInsets.all(8),
-          margin: EdgeInsets.all(8),
-          decoration: BoxDecoration(
-              border: Border.all(color: Color(0xFF002767), width: 8),
-              borderRadius: BorderRadius.circular(30)
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    urunListesi[index].urun_adi,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 24,
-                      decoration: urunListesi[index].urun_alindi
+        GestureDetector(
+          onTap: (){
+            setState(() {
+              urunEkleDialog(
+                  context,
+                  guncellenecekUrun: urunListesi[index],
+                  index: index
+              );
+            });
+          },
+          child: Container(
+            padding: EdgeInsets.all(8),
+            margin: EdgeInsets.all(8),
+            decoration: BoxDecoration(
+                border: Border.all(color: Color(0xFF002767), width: 8),
+                borderRadius: BorderRadius.circular(30)
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      urunListesi[index].urun_adi,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24,
+                        decoration: urunListesi[index].urun_alindi
+                            ? TextDecoration.lineThrough
+                            : TextDecoration.none,
+                        color: urunListesi[index].urun_alindi
+                          ? Colors.grey
+                          : Colors.black
+                      ),
+                    ),
+                    Text(
+                      "${urunListesi[index].urun_adet.toString()} "
+                          "${urunListesi[index].urun_birim}",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        decoration: urunListesi[index].urun_alindi
                           ? TextDecoration.lineThrough
                           : TextDecoration.none,
-                      color: urunListesi[index].urun_alindi
-                        ? Colors.grey
-                        : Colors.black
+                        color: urunListesi[index].urun_alindi
+                          ? Colors.grey
+                          : Colors.black
+                      ),
                     ),
-                  ),
-                  Text(
-                    "${urunListesi[index].urun_adet.toString()} "
-                        "${urunListesi[index].urun_birim}",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      decoration: urunListesi[index].urun_alindi
-                        ? TextDecoration.lineThrough
-                        : TextDecoration.none,
-                      color: urunListesi[index].urun_alindi
-                        ? Colors.grey
-                        : Colors.black
+                  ],
+                ),
+                Row(
+                  children: [
+                    GestureDetector(
+                      onTap: (){
+                        setState(() {
+                          urunListesi[index].urun_alindi = !urunListesi[index].urun_alindi;
+                        });
+                      },
+                      child: Icon(
+                        urunListesi[index].urun_alindi ? Icons.undo : Icons.add,
+                        size: 50,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  GestureDetector(
-                    onTap: (){
-                      setState(() {
-                        urunListesi[index].urun_alindi = !urunListesi[index].urun_alindi;
-                      });
-                    },
-                    child: Icon(
-                      urunListesi[index].urun_alindi ? Icons.undo : Icons.add,
-                      size: 50,
+                    SizedBox(width: 8,),
+                    GestureDetector(
+                      onTap: (){
+                        Fluttertoast.showToast(
+                          msg: "Silmek için uzun basınız",
+                          toastLength: Toast.LENGTH_SHORT,
+                          gravity: ToastGravity.BOTTOM,
+                          backgroundColor: Color(0xFF002767),
+                          textColor: Colors.white
+                        );
+                      },
+                      onLongPress: (){
+                        setState(() {
+                          urunListesi.remove(urunListesi[index]);
+                        });
+                      },
+                      child: Icon(
+                        Icons.delete,
+                        size: 50,
+                      ),
                     ),
-                  ),
-                  SizedBox(width: 8,),
-                  GestureDetector(
-                    onTap: (){
-                      Fluttertoast.showToast(
-                        msg: "Silmek için uzun basınız",
-                        toastLength: Toast.LENGTH_SHORT,
-                        gravity: ToastGravity.BOTTOM,
-                        backgroundColor: Color(0xFF002767),
-                        textColor: Colors.white
-                      );
-                    },
-                    onLongPress: (){
-                      setState(() {
-                        urunListesi.remove(urunListesi[index]);
-                      });
-                    },
-                    child: Icon(
-                      Icons.delete,
-                      size: 50,
-                    ),
-                  ),
-                ],
-              )
-            ],
+                  ],
+                )
+              ],
+            ),
           ),
         )
       ],
+    );
+  }
+
+  Future<void> urunEkleDialog(
+    BuildContext context,
+    {Urun? guncellenecekUrun, int? index}
+  ) async {
+
+    /*
+      Ürün Ekle/Güncelle Dialog Penceresini açar
+     */
+
+    TextEditingController _controllerUrunAdi = TextEditingController();
+    int adetSayaci = 1;
+    String yerelBirim = birim;
+
+    if(guncellenecekUrun != null){
+      _controllerUrunAdi.text = guncellenecekUrun.urun_adi;
+      adetSayaci = guncellenecekUrun.urun_adet;
+      yerelBirim = guncellenecekUrun.urun_birim;
+    }
+
+    return showDialog(
+      context: context,
+      builder: (BuildContext context){
+        return StatefulBuilder(
+          builder:(BuildContext context, _setState){
+            return Dialog(
+              backgroundColor: Color(0xFFADD8E6),
+              child: Container(
+                padding: EdgeInsets.all(8),
+                margin: EdgeInsets.all(8),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  spacing: 8,
+                  children: [
+                    Text(
+                      guncellenecekUrun != null ? "Ürün Güncelleme" : "Ürün Ekleme",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    TextField(
+                      controller: _controllerUrunAdi,
+                      decoration: InputDecoration(
+                        labelText: "Ürün Adı"
+                      ),
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        IconButton(
+                          onPressed: (){
+                            _setState(() {
+                              if(adetSayaci > 1){
+                                adetSayaci--;
+                              }
+                            });
+                          },
+                          icon: Icon(
+                            Icons.remove,
+                            size: 40,
+                          ),
+                        ),
+                        Text(
+                          adetSayaci.toString(),
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: (){
+                            _setState(() {
+                              adetSayaci++;
+                            });
+                          },
+                          icon: Icon(
+                            Icons.add,
+                            size: 40,
+                          ),
+                        )
+                      ],
+                    ),
+                    DropdownButton<String>(
+                      value: yerelBirim,
+                      items: birimListesi.map((String? yeniBirim){
+                        return DropdownMenuItem<String>(
+                          value: yeniBirim,
+                          child: Text(
+                            yeniBirim!,
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold
+                            ),),
+                        );
+                      }).toList(),
+                      onChanged: (String? yeniBirim){
+                        _setState(() {
+                          yerelBirim = yeniBirim!;
+                        });
+                      },
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: ElevatedButton(
+                            onPressed: (){
+                              Navigator.of(context).pop();
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.orange,
+                              foregroundColor: Colors.black
+                            ),
+                            child: Text(
+                              "Vazgeç",
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold
+                              ),),
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: ElevatedButton(
+                            onPressed: (){
+                              setState(() {
+                                Navigator.of(context).pop();
+
+                                if(guncellenecekUrun != null){
+                                  urunListesi[index!] = Urun(
+                                    urun_id: guncellenecekUrun.urun_id,
+                                    urun_adi: _controllerUrunAdi.text,
+                                    urun_birim: yerelBirim,
+                                    urun_adet: adetSayaci
+                                  );
+                                }else{
+                                  Urun yeniUrun = Urun(
+                                    urun_id: urunListesi.length + 1,
+                                    urun_adi: _controllerUrunAdi.text,
+                                    urun_birim: yerelBirim,
+                                    urun_adet: adetSayaci
+                                  );
+
+                                  urunListesi.add(yeniUrun);
+                                }
+
+                              });
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Color(0xFF002767),
+                              foregroundColor: Colors.white
+                            ),
+                            child: Text(
+                              guncellenecekUrun != null ? "Güncelle" : "Ekle",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            );
+          }
+        );
+      },
     );
   }
 }
